540687d6b7d23574effd20c2ccbf01c0cb560fac
TuvalZit18 <18tuvalzit18@gmail.com>
2026-06-07 20:01:58 +0300

    deleting items

--- index.js
+++
@@ -1,9 +0,0 @@
-const getBalance = function (bankOperations) {
-  let balance = 0;
-  for (let op of bankOperations) {
-    balance += op;
-  }
-  return balance;
-};
-
-getBalance([-27, -43, -2400, -700, 15000, 58]);
