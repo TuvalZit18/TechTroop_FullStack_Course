619d0b65191b910b0d3724a5935ce32dc00c452f
TuvalZit18 <18tuvalzit18@gmail.com>
2026-06-23 14:34:11 +0300

    Renaming

---
+++ class_29_react/src/App.jsx
@@ -0,0 +1,15 @@
+import Exe1 from "./classes/react_basics/exe_1/Exe1";
+import Exe2 from "./classes/react_basics/exe_2/Exe2";
+
+function App() {
+  return (
+    <div>
+      <h1>React Basics</h1>
+      <Exe1></Exe1>
+      <Exe2></Exe2>
+      <h1>React Component</h1>
+    </div>
+  );
+}
+
+export default App;
